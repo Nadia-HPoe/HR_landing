ea8f56b2624a141aee52652f8520572953a8a6cd
Nadia-HPoe <heatherpoe.bn@gmail.com>
2025-07-16 09:27:46 +0200

    fix/header-scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,7 +21,13 @@ function Header() {
   const handleScroll = (id: string) => {
     const el = document.getElementById(id);
     if (el) {
-      el.scrollIntoView({ behavior: "smooth", block: "start" });
+      const yOffset = -50;
+      const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+      window.scrollTo({
+        top: y,
+        behavior: "smooth",
+      });
     }
   };
   const closeMenu = () => {
